288a1638080f9324c0e22170f6b3559a727151f6
Iaroslav Karandashev <yarix7@gmail.com>
2024-05-07 15:26:28 -0700

    Create validParenthesis.js

---
+++ validParenthesis.js
@@ -0,0 +1,22 @@
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+var isValid = function(s) {
+  const pairs = {
+    '(': ')',
+    '{': '}',
+    '[': ']',
+  }
+  const stack = [];
+  for (let i = 0; i < s.length; i++) {
+    if (pairs[s[i]]) {
+      stack.push(pairs[s[i]]);
+    } else {
+      if (stack.pop() != s[i]) {
+        return false
+      }
+    }
+  }
+  return stack.length == 0;
+};
